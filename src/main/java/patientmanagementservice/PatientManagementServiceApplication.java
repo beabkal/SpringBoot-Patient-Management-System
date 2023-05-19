ebceab917eef9a1f6e691517d12523c572696dbb
beabkal <b3abkal@gmail.com>
2023-05-19 01:37:20 -0500

    added OpenAPI Swagger UI

--- src/main/java/patientmanagementservice/PatientManagementServiceApplication.java
+++ src/main/java/patientmanagementservice/PatientManagementServiceApplication.java
@@ -1,9 +1,24 @@
 package patientmanagementservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Patient Microservice REST API Documentation",
+				description = "Patient Microservice REST API Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Beabkal",
+						email = "b3abkal@gmail.com",
+						url = "https://github.com/beabkal"
+				)
+		)
+)
 public class PatientManagementServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/patientmanagementservice/controller/PatientController.java
+++ src/main/java/patientmanagementservice/controller/PatientController.java
@@ -1,5 +1,8 @@
 package patientmanagementservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -9,6 +12,11 @@ import patientmanagementservice.dto.PatientDto;
 import patientmanagementservice.service.PatientService;
 import java.util.List;
 
+@Tag(
+        name = "CRUD REST APIs for Patient Resources",
+        description = "CRUD REST APIs - Creat Patient, Update Patient, Get Patient, Get All Patients, Delete Patient " +
+                "Find Patient By PhoneNumber or Email"
+)
 @RestController
 @RequestMapping("api/patients")
 @AllArgsConstructor
@@ -16,18 +24,14 @@ public class PatientController {
 
     private PatientService patientService;
 
-    @GetMapping
-    public ResponseEntity<List<PatientDto>> getAllPatients(){
-        List<PatientDto> patients = patientService.getAllPatients();
-        return new ResponseEntity<>(patients, HttpStatus.OK);
-    }
-
-    @GetMapping("{id}")
-    public ResponseEntity<PatientDto> getPatient(@PathVariable Long id){
-        PatientDto patientDto = patientService.getPatient(id);
-        return new ResponseEntity<>(patientDto, HttpStatus.OK);
-    }
-
+    @Operation(
+            summary = "Create Patient REST API",
+            description = "Create Patient REST API is used to save patient in a database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
     @PostMapping("create")
     public ResponseEntity<PatientDto> createPatient(
             @Valid
@@ -35,7 +39,41 @@ public class PatientController {
         PatientDto savedPatient = patientService.createPatient(patientDto);
         return new ResponseEntity<>(savedPatient, HttpStatus.CREATED);
     }
+    @Operation(
+            summary = "Get All Patients REST API",
+            description = "Get All Patients REST API is used to retrieve all patients from database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 200 SUCCESS"
+    )
+    @GetMapping
+    public ResponseEntity<List<PatientDto>> getAllPatients(){
+        List<PatientDto> patients = patientService.getAllPatients();
+        return new ResponseEntity<>(patients, HttpStatus.OK);
+    }
 
+    @Operation(
+            summary = "Get Patient REST API",
+            description = "Get Patient REST API is used to get a patient from database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 OK"
+    )
+    @GetMapping("{id}")
+    public ResponseEntity<PatientDto> getPatient(@PathVariable Long id){
+        PatientDto patientDto = patientService.getPatient(id);
+        return new ResponseEntity<>(patientDto, HttpStatus.OK);
+    }
+    @Operation(
+            summary = "Update Patient REST API",
+            description = "Update Patient REST API is used to update patient in a database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     @PutMapping("{id}/update")
     public ResponseEntity<PatientDto> updatePatient(
             @Valid
@@ -44,13 +82,27 @@ public class PatientController {
         PatientDto updatedPatient = patientService.updatePatient(patientDto);
         return new ResponseEntity<>(updatedPatient, HttpStatus.OK);
     }
-
+    @Operation(
+            summary = "DELETE Patient REST API",
+            description = "DELETE Patient REST API is used to delete a patient in a database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
     @PostMapping("{id}/delete")
     public ResponseEntity<String> deletePatient(@PathVariable Long id){
         patientService.deletePatient(id);
         return new ResponseEntity<>("Patient Deleted Successfully!", HttpStatus.OK);
     }
-
+    @Operation(
+            summary = "Search Patient by Phone Number or Email REST API",
+            description = "Search a Patient REST API is used to find patient in a database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 201 CREATED"
+    )
     @GetMapping("{phoneNumberOrEmail}/search")
     public ResponseEntity<PatientDto> getPatientByPhoneNumberOrEmail(@PathVariable String phoneNumberOrEmail){
 
